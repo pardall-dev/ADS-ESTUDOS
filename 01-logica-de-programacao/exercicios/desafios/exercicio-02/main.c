4eecd899d31360a1be43314b85384527a3a72ad4
pardall-dev <d.gustavopablo2240@outlook.com.br>
2026-04-08 13:17:15 -0300

    concluido o exercicio 2 de LP

---
+++ 01-logica-de-programacao/exercicios/desafios/exercicio-02/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int nota1 = 10;
+    int nota2 = 15;
+
+    int media = (nota1 + nota2) / 2;
+
+    printf("A média das notas %d e %d foi de %d!\n", nota1, nota2, media);
+    return 0;
+}
